4de6dfb65291c75ed7f452056d9065201625ece6
Daniel Beck <daniel.beck@posteo.de>
2018-06-30 08:13:39 +0200

    Exception handling for null Requests.

--- src/main/java/de/neosearch/rest/RestController.java
+++ src/main/java/de/neosearch/rest/RestController.java
@@ -1,12 +1,20 @@
 package de.neosearch.rest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @org.springframework.web.bind.annotation.RestController
 public class RestController {
 
+	private static final Logger log = LoggerFactory.getLogger(RestController.class);
+
 	@PostMapping
 	public void annotate(AnnotationRequest req) {
+		if (req == null || req.getText() == null)
+			throw new RuntimeException("Nothing to annotate");
+		log.info("Annotating Request : " + req.getText().substring(0, Math.max(15, req.getText().length())) + "...");
 
+		
 	}
 }
