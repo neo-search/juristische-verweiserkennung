9e5996e63028204191db23f9f00458a62b60a6c4
daniel <d.danielbeck@googlemail.com>
2018-07-14 09:59:48 +0200

    REST-API aus Library entfernt.

--- src/main/java/de/neosearch/rest/AnnotationRequest.java
+++
@@ -1,9 +0,0 @@
-package de.neosearch.rest;
-
-import lombok.Data;
-
-@Data
-public class AnnotationRequest {
-	private String text;
-	private String targetPlatform; // urteile-gesetze (default) oder opendata
-}
